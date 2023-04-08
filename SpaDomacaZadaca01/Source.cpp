3a943ced3943b9fac900cc5a2236b7bc1cfb5ee7
enoo b <enwerinho@gmail.com>
2023-04-08 05:45:38 -1100

    first commit

---
+++ SpaDomacaZadaca01/Source.cpp
@@ -0,0 +1,24 @@
+#include <SFML/Graphics.hpp>
+#include "Cvijet.h"
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Moj Cvijet");
+	window.setFramerateLimit(60);
+	Cvijet cvijet(&window); //stvara objekt klase cvijet koji prima window kao argument
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		window.clear();
+		cvijet.draw(); //poziv funkcije ce nacrtati cvijet na window koristeci objekt klase cvijet
+		window.display();
+	}
+
+	return 0;
+}
